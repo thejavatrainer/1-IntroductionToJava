13dc03bc12aa7e14915303dd1ad0165828a5e50f
achiscar <andreea.chiscari@frequentis.com>
2019-08-12 11:21:10 +0300

    Add gitignore. Remove unknown dependency.

--- Exercises/maven/my-maven-app/src/main/java/com/Main.java
+++ Exercises/maven/my-maven-app/src/main/java/com/Main.java
@@ -1,14 +1,11 @@
 package com;
 
-import com.frequentis.ps.conversation.contract.ConversationServiceCode;
-
 public class Main {
     public static void main(String[] args) {
         PhoneBookEntry entry = new PhoneBookEntry();
-        entry.name ="John";
-        entry.telephoneNumber ="0232432";
-
-        System.out.println("Hello from my app: " + ConversationServiceCode.COMMUNICATION_CATEGORY_DOES_NOT_EXIST);
+        entry.name = "John";
+        entry.telephoneNumber = "0232432";
+        System.out.println("Main!");
     }
 
     public String doSomething() {
